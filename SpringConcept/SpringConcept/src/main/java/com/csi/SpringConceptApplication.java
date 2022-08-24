4da3d5d73342a860be26085e7cc380202e786b63
1994V <vaibhavdhanawade83@gmail.com>
2022-08-24 15:13:02 +0530

    code coicode comitted by vaibhav

---
+++ SpringConcept/SpringConcept/src/main/java/com/csi/SpringConceptApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConceptApplication.class, args);
+	}
+
+}
